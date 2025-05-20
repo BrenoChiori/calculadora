fe2314a7eb7a84875953d8ac903cbe7ae6b6cc99
Breno Chiori <112735329+BrenoChiori@users.noreply.github.com>
2025-05-20 09:09:41 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,98 @@
+function teclas(event) {
+    const teclasSelect = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '/', '*', '-', '+', 'Backspace']
+    const teclaEnter = ['Enter']
+
+    if (!teclasSelect.includes(event.key)) {
+        event.preventDefault()
+    }
+
+    if (teclaEnter.includes(event.key)) {
+        calcula()
+    }
+}
+
+function teclasSelecionadas(tecla) {
+    if(tecla == "c") {
+        return document.getElementById("inputCalc").value = ""
+    }
+
+    let currentValue = document.getElementById("inputCalc").value
+    document.getElementById("inputCalc").value = currentValue += tecla
+}
+
+function calcula() {
+    let calculoCompleto = document.getElementById("inputCalc").value
+    let calculoSeparado = calculoCompleto.match(/\d+|[+\-*/]/g)
+    let resultado = 0
+
+    if(calculoSeparado.length <= 2) {
+        return document.getElementById("calc-result").innerText = "Não e possivel realizar essa operação"
+    }
+
+    console.log(`Array inicial ${calculoSeparado}`)
+
+    for (let j = 0; j < calculoSeparado.length; j++) {
+        let valor = calculoSeparado[j]
+        let valor1, valor2
+
+        switch (valor) {
+            case '*':
+                valor1 = parseInt(calculoSeparado[j - 1])
+                valor2 = parseInt(calculoSeparado[j + 1])
+                resultado = valor1 * valor2
+                calculoSeparado[j] = resultado.toString()
+                calculoSeparado.splice(j + 1, 1)
+                calculoSeparado.splice(j - 1, 1)
+                resultado = 0
+                break;
+
+            case '/':
+                valor1 = parseInt(calculoSeparado[j - 1])
+                valor2 = parseInt(calculoSeparado[j + 1])
+                resultado = valor1 / valor2
+                calculoSeparado[j] = resultado.toString()
+                calculoSeparado.splice(j + 1, 1)
+                calculoSeparado.splice(j - 1, 1)
+                resultado = 0
+                break;
+        }
+    }
+
+    console.log(`Array final ${calculoSeparado}`)
+
+    if (calculoSeparado.length > 1) {
+        for (let i = 0; i < calculoSeparado.length; i++) {
+            let valorUnico = calculoSeparado[i]
+            let numero1, numero2
+
+            switch (valorUnico) {
+                case "+":
+                    if (resultado == 0) {
+                        numero1 = parseInt(calculoSeparado[i - 1])
+                    } else {
+                        numero1 = resultado
+                    }
+                    numero2 = parseInt(calculoSeparado[i + 1])
+                    resultado = numero1 + numero2
+                    break;
+
+                case '-':
+                    if (resultado == 0) {
+                        numero1 = parseInt(calculoSeparado[i - 1])
+                    } else {
+                        numero1 = resultado
+                    }
+                    numero2 = parseInt(calculoSeparado[i + 1])
+                    resultado = numero1 - numero2
+                    break;
+            }
+        }
+    } else {
+        resultado = calculoSeparado[0]
+    }
+
+    console.log(`Resultado final ${resultado}`)
+
+    document.getElementById("inputCalc").value = resultado.toString()
+    document.getElementById("calc-result").innerText = `Resultado de ${calculoCompleto} = ${resultado}`
+}
